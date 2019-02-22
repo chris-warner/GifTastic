7692381ed900bc3e055f73f749868dc178b293d1
Chris Warner <chriswarner@Chriss-MBP.home>
2019-02-22 13:40:19 -0500

    added click event for dynamic buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 var apiKey = 'JqrEkMGSAzAKM3HxZtFci1lQzrkd5Db3';
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
-var buttonCategories = ["cats", "dogs", "funny"];
+var buttonCategories = ["cats", "dogs", "funny", "trending"];
 var dynamicButtons = [];
 // $.ajax({
 //     url: queryURL,
@@ -15,14 +15,16 @@ $(document).ready(function() {
     populateButtons(buttonCategories);
 
     function populateButtons(inputarray) {
-
         for (var i = 0; i < inputarray.length; i++) {
             $("#button-div").append("<button id =" + "'" + "btn" + i + "'" + ">" + buttonCategories[i] + "</button>");
             dynamicButtons.push("btn" + i);
         }
+        //2.Log the button names fot testing.
         for (var b = 0; b < dynamicButtons.length; b++) {
             console.log(dynamicButtons[b]);
         }
-        $("#button-div").text();
     }
+    $('button').click(function() {
+        alert($(this).text());
+    });
 });
