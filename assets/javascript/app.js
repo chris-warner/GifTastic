5764ab6cec309c81f9f6e9f50d2a85707c3a2835
Chris Warner <chriswarner@Chriss-MacBook-Pro.local>
2019-02-24 19:28:54 -0500

    fully working!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,7 @@ var dynamicButtons = [];
 var searchTerm;
 var custom_searchTerm;
 var custom_buttonCategories = [];
+var gTable = document.getElementById("gif-table");
 
 $(document).ready(function() {
 
@@ -21,9 +22,8 @@ $(document).ready(function() {
             console.log(dynamicButtons[b]);
         }
     }
-
+    //if a button is clicked we want to pull up related gifs.
     $('#button-div').on('click', 'button', function() {
-
         cleargifs();
         if ($(this).text() != "Submit") {
             searchTerm = $(this).text();
@@ -36,7 +36,23 @@ $(document).ready(function() {
             var test = $("<button>" + custom_searchTerm + "</button>");
             $("#button-div").append(test);
             dynamicButtons.push(test);
-            populateImages(custom_searchTerm);
+        }
+    });
+    //If a gif is clicked, we want to freeze it.
+    $('#gif-div').on('click', 'img', function() {
+        //Retrieve  the clicked gif's give source url.
+        var gifSrc = $(this).attr("src");
+        //check to see if the gif is playing, store play state using a custom class.
+        //Stop the gif.
+        if ($(this).hasClass('playing')) {
+            $(this).attr('src', gifSrc.replace(/\.gif/i, "_s.gif"));
+            $(this).removeClass('playing');
+        }
+        //Play the gif.
+        else {
+            $(this).addClass('playing');
+            $(this).addClass('child');
+            $(this).attr('src', gifSrc.replace(/\_s.gif/i, ".gif"));
         }
     });
 
@@ -49,17 +65,27 @@ $(document).ready(function() {
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response) {
-            console.log(response);
-            for (var g = 0; g < response.data.length; g++) {
-                console.log(response.data[g]);
-                var width = getRandomSize(200, 400);
-                var height = getRandomSize(200, 400);
-                gifurl = response.data[g].images.fixed_height.url;
+        }).then(function(gif_responce) {
+            console.log(gif_responce);
+            for (var g = 0; g < gif_responce.data.length; g++) {
+                console.log(gif_responce.data[g]);
+                gifurl = gif_responce.data[g].images.fixed_height.url;
+                //var rating = respone.data[g].rating;
+                var p = $("<p>").text("Rating: " + gif_responce.data[g].rating);
+                //  p.addClass("animal-info");
                 var gif = $("<img>").attr("src", gifurl);
-                $("#gif-div").hide(); //Prepare for animation!
-                $("#gif-div").append(gif); // Load Gifs
-                $("#gif-div").fadeIn("fast", function() {});
+                //  $(gif).attr('figcaption').text("Rating: " + gif_responce.data[g].rating);
+                var pghtml = ("<figure>" + "<img src=" + "'" + gifurl + "'" + ">" + "</img>" + "<figcaption> " + "Rating: " + gif_responce.data[g].rating + "</figcaption>" + "</figure>");
+                //                alert(p);
+                $(gif).addClass('playing'); //Gif is initially playing. Store that as a class.
+                // Gifcontainer.append(p);
+                //  Gifcontainer.append(gif);
+                // $("#gif-div").hide(); //Prepare for animation!
+                //   $(gif).hide();
+                //  $("#gif-div").append(p); //append rating
+                $("#gif-div").append(pghtml);
+                // $("#gif-div").append(Gifcontainer);
+                //  $(gif).fadeIn("slow", function() {}); //fade in the div.
             }
         });
     }
@@ -72,5 +98,6 @@ $(document).ready(function() {
     function getRandomSize(min, max) {
         return Math.round(Math.random() * (max - min) + min);
     }
+
 
 });
