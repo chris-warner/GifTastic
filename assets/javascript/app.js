7a37b42ca7339e4e939f90638304d110d9ef322a
Chris Warner <chriswarner@Chriss-MacBook-Pro.local>
2019-02-24 19:57:38 -0500

    added style and position emhancments to css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,19 +25,17 @@ $(document).ready(function() {
     //if a button is clicked we want to pull up related gifs.
     $('#button-div').on('click', 'button', function() {
         cleargifs();
-        if ($(this).text() != "Submit") {
-            searchTerm = $(this).text();
-            populateImages(searchTerm);
-        } else if ($(this).text() != "Submit") {
-            cleargifs();
-        }
-        if ($(this).text() === "Submit") {
-            custom_searchTerm = $("#animal-text").val().trim();
-            var test = $("<button>" + custom_searchTerm + "</button>");
-            $("#button-div").append(test);
-            dynamicButtons.push(test);
-        }
+        searchTerm = $(this).text();
+        populateImages(searchTerm);
     });
+    $('#addAnimal-div').on('click', 'button', function() {
+        custom_searchTerm = $("#animal-text").val().trim();
+        var test = $("<button>" + custom_searchTerm + "</button>");
+        $("#button-div").append(test);
+    });
+
+
+
     //If a gif is clicked, we want to freeze it.
     $('#gif-div').on('click', 'img', function() {
         //Retrieve  the clicked gif's give source url.
@@ -45,47 +43,36 @@ $(document).ready(function() {
         //check to see if the gif is playing, store play state using a custom class.
         //Stop the gif.
         if ($(this).hasClass('playing')) {
-            $(this).attr('src', gifSrc.replace(/\.gif/i, "_s.gif"));
+            //change url to retrieve a freeze frame.
+            $(this).attr('src', gifSrc.replace(/\_s.gif/i, ".gif"));
             $(this).removeClass('playing');
         }
         //Play the gif.
         else {
+            //change url to retrieve a gif.
             $(this).addClass('playing');
-            $(this).addClass('child');
-            $(this).attr('src', gifSrc.replace(/\_s.gif/i, ".gif"));
+            $(this).attr('src', gifSrc.replace(/\.gif/i, "_s.gif"));
+
         }
     });
 
 
     function populateImages(queryString) {
-        //Take the input string and search giffy for related gifs.
+        //Take the input string and search giphy for related gifs.
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + queryString + apiKey;
         var gifurl;
-        //alert(queryURL);
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(gif_responce) {
-            console.log(gif_responce);
             for (var g = 0; g < gif_responce.data.length; g++) {
                 console.log(gif_responce.data[g]);
                 gifurl = gif_responce.data[g].images.fixed_height.url;
-                //var rating = respone.data[g].rating;
                 var p = $("<p>").text("Rating: " + gif_responce.data[g].rating);
-                //  p.addClass("animal-info");
                 var gif = $("<img>").attr("src", gifurl);
-                //  $(gif).attr('figcaption').text("Rating: " + gif_responce.data[g].rating);
                 var pghtml = ("<figure>" + "<img src=" + "'" + gifurl + "'" + ">" + "</img>" + "<figcaption> " + "Rating: " + gif_responce.data[g].rating + "</figcaption>" + "</figure>");
-                //                alert(p);
-                $(gif).addClass('playing'); //Gif is initially playing. Store that as a class.
-                // Gifcontainer.append(p);
-                //  Gifcontainer.append(gif);
-                // $("#gif-div").hide(); //Prepare for animation!
-                //   $(gif).hide();
-                //  $("#gif-div").append(p); //append rating
-                $("#gif-div").append(pghtml);
-                // $("#gif-div").append(Gifcontainer);
-                //  $(gif).fadeIn("slow", function() {}); //fade in the div.
+                $(pghtml).addClass('playing'); //Gif is initially playing. Store that as a class.
+                $("#gif-div").append(pghtml); //Append image object to gif-div.
             }
         });
     }
